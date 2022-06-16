6c1f02979d82c045c7c4c477916cc4532c77f9c6
Jelmerw22 <70368666+Jelmerw22@users.noreply.github.com>
2022-06-16 15:02:30 +0200

    button

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <!DOCTYPE html>
-
 <html>
-
 <?php require_once "config.php" ?>
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,11 +10,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
-
-
-
+    <button type="button" class="btn btn-primary"><a href="http://localhost/GitHub/CRUDJE/form.php"><p class="color-black">Open Form</p></a></button>
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
