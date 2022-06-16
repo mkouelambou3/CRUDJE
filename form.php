89588c0fab729394b4073d565984ad3baf72fb6c
Zorro <99554280+ZorroDB@users.noreply.github.com>
2022-06-16 15:11:46 +0200

    update

--- form.php
+++ form.php
@@ -1,43 +1,45 @@
 <!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="style.css">
-      <title>`CURD`</title>
-      <style>
-        body{
-          background-color:#2B3E50;
-          width: 600px; 
-          margin: 20px auto;
-        }
-        
-      </style>
-    </head>
-  <body>
-    <h2 class="formText">Nieuwe melding te late student</h2>
-    <div class="container">
-      <form action="index.php">
+<html lang="en">
 
-        <label for="fname">Naam student</label>
-        <input type="text" id="fname" name="firstname" placeholder="Naam..">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>`CURD`</title>
+  <style>
+    body {
+      background-color: #2B3E50;
+      width: 600px;
+      margin: 20px auto;
+    }
+  </style>
+</head>
 
-        <label for="lname">Klas</label>
-        <input type="text" id="lname" name="lastname" placeholder="Klas..">
+<body>
+  <h2 class="formText">Nieuwe melding te late student</h2>
+  <div class="container">
+    <form action="index.php">
 
-        <label for="country">Minuten te laat</label>
-        <select id="country" name="country">
-          <option value="5">5</option>
-          <option value="10">10</option>
-          <option value="15">15</option>
-        </select>
+      <label for="fname">Naam student</label>
+      <input type="text" id="fname" name="firstname" placeholder="Naam..">
 
-        <label for="subject">Reden te laat</label>
-        <textarea id="subject" name="subject" placeholder="Reden te laat.." style="height:200px"></textarea>
+      <label for="lname">Klas</label>
+      <input type="text" id="lname" name="lastname" placeholder="Klas..">
 
-        <input type="submit" value="Opslaan">
-      </form>
-    </div>
-  </body>
+      <label for="country">Minuten te laat</label>
+      <select id="country" name="country">
+        <option value="5">05</option>
+        <option value="10">10</option>
+        <option value="15">15</option>
+      </select>
+
+      <label for="subject">Reden te laat</label>
+      <textarea id="subject" name="subject" placeholder="Reden te laat.." style="height:200px"></textarea>
+
+      <input type="submit" value="Opslaan">
+    </form>
+  </div>
+</body>
+
 </html>
