8c0994d3915aad4491327d55a96c22d0874142b9
Jelmerw22 <70368666+Jelmerw22@users.noreply.github.com>
2022-06-16 14:38:25 +0200

    update

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>CRUD ultimate daddy</title>
+    <title>Kutje Crudje</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="style.css">
