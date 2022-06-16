499cbc4f807f8ff3e3cbcdd1c5becfd2d3c2ccd4
Jelmerw22 <70368666+Jelmerw22@users.noreply.github.com>
2022-06-16 15:06:01 +0200

    update

--- index.php
+++ index.php
@@ -11,7 +11,11 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <button type="button" class="btn btn-primary"><a href="http://localhost/GitHub/CRUDJE/form.php"><p class="color-black">Open Form</p></a></button>
+    <div class="row">
+        <div class="col-md-6">
+        <a class="btn btn-primary" href="form.php" role="button">Form</a>
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
